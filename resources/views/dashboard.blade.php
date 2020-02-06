42fe50fbd58919af668f407e72a8636715b726a3
Alex Caloggero <a.calog@protonmail.com>
2020-02-05 23:33:54 -0500

    v1.0.7; Updated transaction input style

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,21 +32,38 @@
             <div class='col-md-8'>
                 <fieldset class='shadow' id='manual-input-form'>
                     <div class='form-group'>
-                        <label for='description-input'>Transaction Description:</label>
-                        <input type='text' class='manual-input' name='description' id='description-input'>
+                        <div class='col-md-6 offset-md-4'>
+                            <label class='w-100 mb-0' for='description-input'>Transaction Description:</label>
+                            <input type='text' class='manual-input' name='description' id='description-input'>
+                        </div>
                     </div>
-
-                    <label for='amount-input'>Amount $:</label>
-                    <input type='number' step='0.01' class='manual-input' id='amount-input' name='amount'>
-                    <label for='manual-select-category'>Category</label>
-                    <select name='category' id='manual-select-category' class='manual-input'>
-                        @foreach($categories as $category)
-                            <option value='{{ $category }}'>{{ $category }}</option>
-                        @endforeach
-                    </select>
-                    <label for='transaction-date-input'>Transaction Date:</label>
-                    <input type='date' class='manual-input' id='transaction-date-input' name='transaction_date'>
-                    <button class='manual-input' id='manual-input-button' type='submit'>Save</button>
+                    <div class='form-group'>
+                        <div class='col-md-6 offset-md-4'>
+                            <label for='amount-input'>Amount $:</label>
+                            <input type='number' step='0.01' class='manual-input' id='amount-input' name='amount'>
+                        </div>
+                    </div>
+                    <div class='form-group'>
+                        <div class='col-md-6 offset-md-4'>
+                            <label for='manual-select-category'>Category</label>
+                            <select name='category' id='manual-select-category' class='manual-input'>
+                                @foreach($categories as $category)
+                                    <option value='{{ $category }}'>{{ $category }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <div class='form-group'>
+                        <div class='col-md-6 offset-md-4'>
+                            <label for='transaction-date-input'>Transaction Date:</label>
+                            <input type='date' class='manual-input' id='transaction-date-input' name='transaction_date'>
+                        </div>
+                    </div>
+                    <div class='form-group'>
+                        <div class='col-md-6 offset-md-4'>
+                            <button class='manual-input btn btn-outline-success btn-block' id='manual-input-button' type='submit'>Save</button>
+                        </div>
+                    </div>
                 </fieldset>
             </div>
         </div>
