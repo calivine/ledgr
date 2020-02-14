327ae833759947d3001a0c3ed36ad6f3376ca69d
Alex Caloggero <a.calog@protonmail.com>
2020-02-14 01:22:44 -0500

    v1.0.16; Show/Hide Progress Bars

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,13 +10,22 @@
         </div>
         <div class='row justify-content-center'>
             <div class='col-md-8'>
-                <p class='progress-bar-label text-left'>Total Spending:</p><p class='progress-bar-label float-right'>${{ $monthly_expenditure }} of ${{ $monthly_budget }}</p>
+                <p class='progress-bar-label text-left'>Total Spending:</p><p class='progress-bar-label float-right'>${{ round($monthly_expenditure) }} of ${{ $monthly_budget }}</p>
                 @include('modules.progress-bar', ['monthly_budget' => $monthly_budget, 'monthly_expenditure' => $monthly_expenditure, 'percent' => $budget_percent])
+            </div>
+        </div>
+        <div class='row justify-content-center m-3'>
+            <div class='col-md-8'>
+                <button class='btn btn-outline-info btn-block' type='button' data-toggle='collapse' data-target='#collapseBody' aria-expanded='false' aria-controls='collapseBody'>
+                    Details
+                </button>
             </div>
         </div>
         <div class='row justify-content-center'>
             <div class='col-md-8'>
-                @include('modules.budget-progress-bars', $budget)
+                <div class='collapse' id='collapseBody'>
+                    @include('modules.budget-progress-bars', $budget)
+                </div>
             </div>
         </div>
         <div class='row justify-content-center'>
