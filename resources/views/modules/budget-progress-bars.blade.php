bbc8e107304a1b77eb319117f064e8fdc60ebaa6
Alex Caloggero <a.calog@protonmail.com>
2020-02-13 23:56:17 -0500

    v1.0.15; Minor change

--- resources/views/modules/budget-progress-bars.blade.php
+++ resources/views/modules/budget-progress-bars.blade.php
@@ -2,7 +2,7 @@
     @if($row->planned > 0)
         <p class='progress-bar-label text-left'>{{ $row->category }}</p><p class='progress-bar-label float-right'>${{ $row->actual }} of ${{ $row->planned }}</p>
         <div class='progress' style='height: 20px;'>
-            <div class='progress-bar bg-success' role='progressbar' style='width: {{ ($row->actual / $row->planned) * 100 }}%' aria-valuenow='{{ $row->actual }}' aria-valuemin='0' aria-valuemax='{{ $row->planned }}'>{{ ($row->actual / $row->planned) * 100 }}%</div>
+            <div class='progress-bar bg-success' role='progressbar' style='width: {{ ($row->actual / $row->planned) * 100 }}%' aria-valuenow='{{ $row->actual }}' aria-valuemin='0' aria-valuemax='{{ $row->planned }}'>{{ round(($row->actual / $row->planned) * 100) }}%</div>
         </div>
     @endif
 @endforeach
