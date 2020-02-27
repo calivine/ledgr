fb4787f13ed52194ec1da36b4d9f33380a54f40e
Alex Caloggero <a.calog@protonmail.com>
2020-02-27 15:23:59 -0500

    v1.5.0; Dashboard Style Update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,26 +15,14 @@
                 @include('modules.budget-progress-bars', $budget)
             </div>
         </div>
-        <div class='row justify-content-center m-3'>
-            <div class='col-md-8'>
-                <button class='btn btn-outline-info btn-block' type='button' data-toggle='collapse' data-target='#collapseBody' aria-expanded='false' aria-controls='collapseBody'>View Chart</button>
-            </div>
-        </div>
         <div class='row justify-content-center'>
-            <div class='col-md-4'>
-                <div class='collapse' id='collapseBody'>
-                    @include('modules.actuals-pie-chart', ['categories' => $categories, 'actuals' => $actuals])
-                </div>
+            <div class='col-md-8'>
+                @include('modules.accordion', ['category_form_labels' => $category_form_labels, 'categories' => $categories, 'actuals' => $actuals])
             </div>
         </div>
         <div class='row justify-content-center' id='trans-table'>
             <div class='col-md-8'>
                 @include('modules.transactions-table', ['transactions' => $transactions, 'categories' => $category_form_labels])
-            </div>
-        </div>
-        <div class='row justify-content-center m-3'>
-            <div class='col-md-8'>
-                @include('modules.accordion', ['category_form_labels' => $category_form_labels])
             </div>
         </div>
     </div>
--- resources/views/modules/accordion.blade.php
+++ resources/views/modules/accordion.blade.php
@@ -3,12 +3,28 @@
         <div class="card-header" id="headingOne">
             <h2 class="mb-0">
                 <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Enter New Transaction
+                    Show Chart
                 </button>
             </h2>
         </div>
 
         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+            <div class="card-body">
+                @include('modules.actuals-pie-chart', ['categories' => $categories, 'actuals' => $actuals])
+
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h2 class="mb-0">
+                <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    New Transaction
+                </button>
+            </h2>
+        </div>
+
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionExample">
             <div class="card-body">
                 @include('modules.input-transaction', ['categories' => $category_form_labels])
             </div>
