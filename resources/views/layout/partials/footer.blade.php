9a85aa72f7bcfe6f3772c67828861feb0d84d3e6
Alex Caloggero <a.calog@protonmail.com>
2020-03-13 14:37:27 -0400

    v1.10.3; Footer size change

--- resources/views/layout/partials/footer.blade.php
+++ resources/views/layout/partials/footer.blade.php
@@ -1,9 +1,8 @@
+
 <footer class="text-muted">
     <div class="container">
-        <p>&copy; {{ date('Y') }}</p>
-        <p class="float-right">
-            <a href="#">Back to top</a>
-        </p>
+        <p class="mx-auto">&copy; {{ date('Y') }}</p>
+        <p class="float-right mr-1"><a href="#">Back to top</a></p>
     </div>
 </footer>
 
