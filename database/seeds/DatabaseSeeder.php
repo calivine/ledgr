c7d7e5782b084fc989f1a6dc4ebc27895bc131dd
Alex Caloggero <a.calog@protonmail.com>
2020-05-06 14:04:57 -0400

    Added Icons table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
         // $this->call(BudgetTableSeeder::class);
+        $this->call(IconsTableSeeder::class);
     }
 }
---
+++ database/seeds/IconsTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class IconsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $json = file_get_contents('database/data.json');
+        $json_data = json_decode($json, true);
+        foreach($json_data as $index => $icon) {
+            DB::table('icons')->insert(
+                ['text' => $icon]
+            );
+        }
+    }
+}
