aba08b0abbdca9e9fd1841386500fabe5f4596f9
Alex Caloggero <a.calog@protonmail.com>
2020-11-10 19:18:59 -0500

    Registration validation #31

--- resources/js/validator.js
+++ resources/js/validator.js
@@ -1,11 +1,11 @@
 $(function () {
-    $('input#password.form-control.w-50').on('change', function () {
+    $('input#password.form-control').on('change', function () {
         console.log($(this).val());
         let inputText = $(this).val();
         minLength(inputText) ? pass() : reject();
     });
 
-    $('input#password.form-control.w-50').on('keypress', function () {
+    $('input#password.form-control').on('keypress', function () {
 
         console.log($(this).val());
         let inputText = $(this).val();
