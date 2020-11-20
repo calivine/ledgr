c7806a458279ff5ea02804364e579bcf46ed086f
Alex Caloggero <a.calog@protonmail.com>
2020-11-20 13:41:07 -0500

    Api routes added

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,16 +9,6 @@ use Log;
 
 class ApiController extends Controller
 {
-    public function store(Request $request)
-    {
-        Log::info('Storing activity');
-        $activity = Activities::storeTransaction($request);
-
-        return response()->json([
-            "result" => $activity
-        ], 200);
-
-    }
 
     public function getTransactionsByDate(Request $request)
     {
@@ -34,5 +24,35 @@ class ApiController extends Controller
 
         return new ActivityCollection($transactions);
 
+    }
+
+    public function store(Request $request)
+    {
+        Log::info('Storing activity');
+        $activity = Activities::storeTransaction($request);
+
+        return response()->json([
+            "result" => $activity
+        ], 200);
+
+    }
+
+    public function total(Request $request)
+    {
+        $user = $request->user()->id;
+        // Get to and from dates from URL.
+        preg_match_all('/[\d]{4}-[\d]{1,2}-[\d]{1,2}/', $request->url(), $dates);
+
+        $from = $dates[0][0];
+        $to = $dates[0][1];
+
+        $transactions = Activities::getActivitiesByDate($from, $to, $user);
+
+        return $transactions->sum('amount');
+    }
+
+    public function user(Request $request)
+    {
+        return $request->user();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,14 +16,28 @@ use App\Http\Resources\ActivityCollection;
 |
 */
 
+Route::group(['middleware' => 'auth:api'], function() {
+
+    Route::get('/user', 'ApiController@user');
+
+    Route::get('/activity/transactions/{start}/{stop}/', 'ApiController@getTransactionsByDate');
+
+    Route::get('/activity/transactions/{start}/{stop}/total/', 'ApiController@total');
+
+    Route::post('/activity/transaction/create', 'ApiController@store');
+
+
+});
+
 /**
  * Authenticated API routes require: ledgr.site/api/params?api_token=TOKEN
  * Get user by API key: http://ledgr.loc/api/user?api_token=TOKEN
  */
-
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 /*
 Route::get('/activity/transactions', function () {
     return new ActivityCollection(Activity::all());
@@ -33,6 +47,6 @@ Route::middleware('auth:api')->get('/activity/transactions/{start}/{stop}/', fun
     return new ActivityCollection(Activity::whereBetween('date', [$start, $stop])->get());
 });
 */
-Route::middleware('auth:api')->get('/activity/transactions/{start}/{stop}/', 'ApiController@getTransactionsByDate');
+// Route::middleware('auth:api')->get('/activity/transactions/{start}/{stop}/', 'ApiController@getTransactionsByDate');
 
-Route::middleware('auth:api')->post('/activity/transaction/create', 'ApiController@store');
+// Route::middleware('auth:api')->post('/activity/transaction/create', 'ApiController@store');
