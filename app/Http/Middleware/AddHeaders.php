91281c2e4502b85ee3ef0909f30e13d4cbd51639
Alex Caloggero <a.calog@protonmail.com>
2021-01-08 22:48:01 -0500

    Added headers middleware

---
+++ app/Http/Middleware/AddHeaders.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AddHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        // Perform action
+        $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
+        $response->header('Pragma', 'no-cache');
+        $response->header('Expires', '-1');
+        $response->header('Strict-Transport-Security', 'max-age=31536000; includeSubdomains');
+        $response->header('X-Content-Type-Options', 'nosniff');
+        $response->header('X-Frame-Options', 'SAMEORIGIN');
+        $response->header('X-XSS-Protection', '1;mode=block');
+        return $response;
+    }
+}
