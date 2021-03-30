bd3e75b42c583778fb5204b40e44e0b5baae1ae5
Alex Caloggero <a.calog@protonmail.com>
2021-03-29 23:18:14 -0400

    Updated charts

--- resources/views/modules/line-chart.blade.php
+++ resources/views/modules/line-chart.blade.php
@@ -62,6 +62,13 @@
                         return '$'+value;
                     }
                 }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        display: false
+                    }
+                }]
             }
         }
     });
