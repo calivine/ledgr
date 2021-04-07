b8dd4e1eae94db71cfb797589433979ee56160fb
Alex Caloggero <a.calog@protonmail.com>
2021-04-06 21:33:46 -0400

    Headers

--- app/Http/Middleware/AddHeaders.php
+++ app/Http/Middleware/AddHeaders.php
@@ -21,7 +21,7 @@ class AddHeaders
         $response->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate');
         $response->header('Pragma', 'no-cache');
         $response->header('Expires', '-1');
-        $response->header('Strict-Transport-Security', 'max-age=31536000');
+        // $response->header('Strict-Transport-Security', 'max-age=31536000');
         $response->header('X-Content-Type-Options', 'nosniff');
         $response->header('X-Frame-Options', 'SAMEORIGIN');
         $response->header('X-XSS-Protection', '1;mode=block');
